977717eaea749b4a335059047652624854457352
yuki <38411009+yukijina@users.noreply.github.com>
2025-11-21 06:00:15 -0800

    added menu introduction and fragment

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -6,13 +6,22 @@ export default function Menu() {
   return (
     <main>
       <h2>Our Menu</h2>
-      <div>
-        {pizzaData.length !== 0 ? (
-          pizzaData.map((pizza) => <Pizza pizza={pizza} key={pizza.name} />)
-        ) : (
-          <p>We're still working on our menu. Please come back later 🍕</p>
-        )}
-      </div>
+      {pizzaData.length !== 0 ? (
+        <>
+          <p>
+            Authentic Italian Cuisine. 6 creative dishes to choose from. All
+            from our storne oven, all organic, all delicious.
+          </p>
+
+          <>
+            {pizzaData.map((pizza) => (
+              <Pizza pizza={pizza} key={pizza.name} />
+            ))}
+          </>
+        </>
+      ) : (
+        <p>We're still working on our menu. Please come back later 🍕</p>
+      )}
     </main>
   );
 }
